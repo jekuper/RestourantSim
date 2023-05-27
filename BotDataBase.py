06a5806a115f9a9455a49b5c3cec776a47812686
Jekuper <jekkyper1121@gmail.com>
2023-05-27 11:57:08 -0400

    db fixed

--- BotDataBase.py
+++ BotDataBase.py
@@ -1,16 +1,20 @@
 from BotConfigs import HOSTNAME, PORT, USERNAME, PASSWORD, DATABASE_NAME
 from sqlalchemy.engine import Engine
 from sqlalchemy.ext.declarative import declarative_base
-from sqlalchemy import create_engine, Integer, BigInteger, String, \
+from sqlalchemy import create_engine, Integer, BigInteger, Enum, String, \
     Column
-from sqlalchemy.orm import Session
+import enum
+from sqlalchemy.orm import Session, sessionmaker
 
 from BotLocalization import LOCALIZATIONS
 
 Base = declarative_base()
 
 engine : Engine = None
-session : Session = None
+
+class job_types(enum.Enum):
+    chief = 1
+    servant = 2
 
 class user_settings(Base):
     __tablename__ = 'user_settings'
@@ -22,16 +26,26 @@ class restourant(Base):
     id = Column(Integer, primary_key=True)
     user_id = Column(BigInteger)
     name = Column(String(255))
+    income = Column(BigInteger)
+class human_deal(Base):
+    __tablename__ = 'human_deals'
+    id = Column(Integer, primary_key=True)
+    name = Column(String(255))
+    last_name = Column(String(255))
+    job_type = Column(Enum(job_types))
+    cost = Column(Integer)
+    income = Column(Integer)
+    minimum_income = Column(Integer)
 
 
 def Connect() -> Engine:
-    global engine, session
-    engine = create_engine("mysql+mysqldb://"+USERNAME+":"+PASSWORD+"@"+HOSTNAME+":"+str(PORT)+"/"+DATABASE_NAME)
-    session = Session(bind=engine)
+    global engine
+    engine = create_engine("mysql+mysqldb://"+USERNAME+":"+PASSWORD+"@"+HOSTNAME+":"+str(PORT)+"/"+DATABASE_NAME, pool_size=10, pool_pre_ping=True)
+    
     return engine
 
 def update_user_settings(user_id: int, language: str):
-    global session
+    session = Session(bind=engine)
     settings = session.query(user_settings).filter(user_settings.user_id==user_id).first()
     if settings is None:
         return
@@ -41,14 +55,14 @@ def update_user_settings(user_id: int, language: str):
     session.commit()
 
 def get_user_language(user_id: int) -> str:
-    global session
+    session = Session(bind=engine)
     settings = session.query(user_settings).filter(user_settings.user_id==user_id).first()
     if settings is None:
         return None
     return settings.language
 
 def insert_new_user(user_id: int) -> bool:
-    global session
+    session = Session(bind=engine)
     settings = session.query(user_settings).filter(user_settings.user_id==user_id).first()
     if settings is None:
         settings = user_settings(
@@ -65,10 +79,23 @@ def insert_new_user(user_id: int) -> bool:
     return False
 
 def set_restourant_name(user_id: int, name: str):
-    global session
-    rest = session.query(restourant).filter(user_settings.user_id==user_id).first()
+    session = Session(bind=engine)
+    rest = session.query(restourant).filter(restourant.user_id==user_id).first()
     if rest is None:
         return
     rest.name = name
     session.add(rest)
     session.commit()
+
+def get_income (user_id: int) -> int:
+    session = Session(bind=engine)
+    rest = session.query(restourant.income).filter(restourant.user_id==user_id).first()
+    if rest is None:
+        return 0
+    return rest[0]
+
+def get_available_human_deals (user_id: int, job_type: job_types):
+    session = Session(bind=engine)
+    deals = session.query(human_deal).filter(human_deal.job_type == job_type, human_deal.minimum_income <= get_income(user_id)).all()
+    return deals
+    
